3192e09ff64d5d45b5b2e237a0527a7e1c3b91a7
javier.arias <javier.arias@skyscanner.net>
2018-04-25 12:27:22 +0200

    kafka listening on all network ifaces, dummy EnricherTask

--- bin/start_kafka.sh
+++ bin/start_kafka.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-docker run --rm --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 -p 2181:2181 -p 9092:9092 --name kafka -h kafka spotify/kafka
+docker run --rm --env ADVERTISED_HOST=kafka --env ADVERTISED_PORT=9092 -p 0.0.0.0:2181:2181 -p 0.0.0.0:9092:9092 --name kafka -h kafka spotify/kafka
 
--- simple-samza-job/src/main/java/metamorphosis/EnricherTask.java
+++ simple-samza-job/src/main/java/metamorphosis/EnricherTask.java
@@ -4,10 +4,24 @@ import org.apache.samza.system.IncomingMessageEnvelope;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.StreamTask;
 import org.apache.samza.task.TaskCoordinator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class EnricherTask implements StreamTask {
+
+    private static Logger log = LoggerFactory.getLogger(EnricherTask.class);
+
     @Override
-    public void process(IncomingMessageEnvelope incomingMessageEnvelope, MessageCollector messageCollector, TaskCoordinator taskCoordinator) throws Exception {
+    public void process(IncomingMessageEnvelope incomingMessageEnvelope,
+        MessageCollector messageCollector, TaskCoordinator taskCoordinator) throws Exception {
+
+        String key = incomingMessageEnvelope.getKey().toString();
+        String msg = incomingMessageEnvelope.getMessage().toString();
+        String offset = incomingMessageEnvelope.getOffset();
+
+        log.info("====>>>>>>>>> received a message. key={} msg={}", key, msg);
+
+        log.info("<<<<<<<<<==== handled message.");
 
     }
 }
