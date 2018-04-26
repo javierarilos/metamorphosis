0bcde9bc8a45788a28c6ad1712056aacd7f4853b
javier.arias <javier.arias@skyscanner.net>
2018-04-26 10:13:43 +0200

    mv EnricherTask -> PhraseSplitterTask. subscribe to phrases-topic, publish to words-topic

--- simple-samza-job/src/main/java/metamorphosis/PhraseSplitterTask.java
+++ simple-samza-job/src/main/java/metamorphosis/PhraseSplitterTask.java
@@ -1,15 +1,19 @@
 package metamorphosis;
 
+import java.util.Arrays;
 import org.apache.samza.system.IncomingMessageEnvelope;
+import org.apache.samza.system.OutgoingMessageEnvelope;
+import org.apache.samza.system.SystemStream;
 import org.apache.samza.task.MessageCollector;
 import org.apache.samza.task.StreamTask;
 import org.apache.samza.task.TaskCoordinator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EnricherTask implements StreamTask {
+public class PhraseSplitterTask implements StreamTask {
 
-    private static Logger log = LoggerFactory.getLogger(EnricherTask.class);
+    private static Logger log = LoggerFactory.getLogger(PhraseSplitterTask.class);
+    private static final SystemStream wordsOutputStream = new SystemStream("kafka-system", "words-topic");
 
     @Override
     public void process(
@@ -34,7 +38,13 @@ public class EnricherTask implements StreamTask {
         if (incomingMessageEnvelope.getMessage() == null) {
             log.warn("!!!!!!!!!!!!!!!!!!!!!!!!!!!!! incomingMessageEnvelope.getMessage() == null");
         } else {
-            log.info(">>>>>>> MSG={}", incomingMessageEnvelope.getMessage().toString());
+            String phrase = incomingMessageEnvelope.getMessage().toString();
+            log.info(">>>>>>> MSG={}", phrase);
+
+            Arrays.stream(phrase.split("\\s+"))
+                .map(word -> new OutgoingMessageEnvelope(wordsOutputStream, word, word))
+                .forEach(msg -> messageCollector.send(msg));
+
         }
 
         log.info(">>>>>>> OFFSET={}", incomingMessageEnvelope.getOffset());
